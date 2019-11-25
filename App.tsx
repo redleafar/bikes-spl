0c2fcfc15ea02e4098b5ca618e32932ec3906795
Rafael <raf_berm2@yahoo.com>
2019-11-25 10:37:49 -0500

    Added code push part

--- App.tsx
+++ App.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import {NativeModules} from 'react-native';
 import Navigator from "./src/screens/Navigator";
 import { NavigationScreenProp } from 'react-navigation';
+import codePush from "react-native-code-push";
+import { AppRegistry } from "react-native";
+import AppContainer from "./App";
 
 interface Props { }
 
@@ -34,7 +37,19 @@ export default class App extends React.Component<Props, State> {
     }
   }
   
-  async function getFlavor() {
-    var {flavor} = await NativeModules.Flavor.getFlavor()
-    App.flavor = flavor;
-  }
+async function getFlavor() {
+  var {flavor} = await NativeModules.Flavor.getFlavor()
+  App.flavor = flavor;
+}
+
+  // Below is an example of how CodePush behaviour can be made different in production: in this case, update will be installed to prod devices only after user has stopped using it for 2 minutes.
+
+const codePushOptions = {
+  checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
+  installMode: codePush.InstallMode.ON_NEXT_SUSPEND,
+  minimumBackgroundDuration: 120
+};
+
+const CodePushedApp = codePush(codePushOptions)(AppContainer);
+AppRegistry.registerComponent("bikesspl", () => CodePushedApp);
+// Third Commit
