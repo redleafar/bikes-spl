0e2b128306a287eb5f7b691f4075c0105d140ddd
Rafael <raf_berm2@yahoo.com>
2019-11-26 23:24:20 -0500

    Some changes for tests

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -8,17 +8,6 @@ import App from '../App';
 // Note: test renderer must be required after react-native.
 import * as renderer from 'react-test-renderer';
 
-jest.mock('react-native', () => {
-  return { 
-    StyleSheet: {
-      create: () => {
-        
-      }
-    },
-    Platform: {}
-  }
-});
-
 it('renders correctly', () => {
   renderer.create(<App />);
 });
